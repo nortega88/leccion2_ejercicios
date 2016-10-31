b3ee972df9ea73ac380f7b8ca4fb2904061c5444
Nestor Ortega Parra <nestorortegap@gmail.com>
2016-10-31 18:28:03 -0300

    Initial Commit

---
+++ Leccion2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Leccion2
+//
+//  Created by Nestor Ortega on 31-10-16.
+//  Copyright © 2016 NextU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
